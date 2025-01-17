f70a5c65bfadd6a04b24a839ca47b3bcd6f8a56e
Mamadou MARÉGA <hamshakour93@gmail.com>
2025-01-17 21:48:48 +0100

    Added paginator

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,6 +15,24 @@ class ArticleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Article::class);
+    }
+
+    /**
+     * @param int $currentPage
+     * @param int $limit
+     * @return Paginator
+     */
+    public function findPaginated(int $currentPage, int $limit = 10)
+    {
+        $query = $this
+            ->createQueryBuilder('a')
+            ->orderBy('a.dateCreated', 'ASC')
+            ->getQuery()
+            ->setFirstResult(($currentPage - 1) * $limit)
+            ->setMaxResults($limit)
+        ;
+
+        return new Paginator($query, true);
     }
 
     //    /**
